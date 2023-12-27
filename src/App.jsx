ff1693a9fe9b0b1d1df32f85c5f6c7955e5ac04e
anwinj <ajt.anwin@gmail.com>
2023-12-27 12:12:59 +0530

    v1.0

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Counter from './components/Counter'
+
+function App() {
+
+  return (
+    <>
+     
+      <div style={{width:'100%',height:'100vh'}} className='w-100 d-flex justify-content-center align-items-center'>
+          <Counter />
+      </div>
+
+    </>
+  )
+}
+
+export default App
